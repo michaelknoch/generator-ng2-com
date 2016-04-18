85580a81f6b31875ff089f237734b49312ad2d0b
Michael Knoch <info@michaelknoch.de>
2016-04-18 16:33:29 +0200

    improve component template

--- generators/app/templates/_component.ts
+++ generators/app/templates/_component.ts
@@ -1,13 +1,17 @@
 import {Component} from 'angular2/core'
-import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators,AbstractControl} from "angular2/common";
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+declare var __moduleName: any;
 
 @Component({
+    moduleId: __moduleName,
     selector: '<%= selector %>',
     directives: [FORM_DIRECTIVES, ROUTER_DIRECTIVES],
-    template: require('./<%= fileName %>.html'),
-    styles: [require('./<%= fileName %>.scss')],
+    template: require('<%= fileName %>.html'),
+    styles: [require('<%= fileName %>.scss')],
 })
+
 export class <%= className %> {
-    constructor(private fb: FormBuilder) {}
+    
+    constructor() {}
+
 }
