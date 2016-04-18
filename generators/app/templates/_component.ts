39057df839e221f089a82ba2fe98751e0a8291ba
Michael Knoch <info@michaelknoch.de>
2016-04-19 01:15:12 +0200

    prevent blank lines

--- generators/app/templates/_component.ts
+++ generators/app/templates/_component.ts
@@ -1,13 +1,13 @@
-import {Component} from 'angular2/core'
-<% if (sugar.router) { %> import {ROUTER_DIRECTIVES} from 'angular2/router'; <% } %>
+import {Component} from 'angular2/core'<% if (sugar.router) { %> 
+import {ROUTER_DIRECTIVES} from 'angular2/router';<% } %>
 declare var __moduleName: any;
 
 @Component({
     moduleId: __moduleName,
-    selector: '<%= selector %>',
-    <% if (sugar.router) { %> directives: [ROUTER_DIRECTIVES], <% } %>
-    <% if (sugar.html) { %> templateUrl: '<%= fileName %>.html', <% } %>
-    <% if (sugar.scss) { %> styleUrls: ['<%= fileName %>.css'], <% } %>
+    selector: '<%= selector %>',<% if (sugar.router) { %> 
+    directives: [ROUTER_DIRECTIVES], <% } %><% if (sugar.html) { %> 
+    templateUrl: '<%= fileName %>.html', <% } %><% if (sugar.scss) { %> 
+    styleUrls: ['<%= fileName %>.css'], <% } %>
 })
 
 export class <%= className %> {
