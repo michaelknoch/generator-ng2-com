9ee43e128e50873f53cb8f3bb4e9b11757c8e5b7
Michael Knoch <info@michaelknoch.de>
2016-05-11 11:54:36 +0200

    Update _component.ts

--- generators/app/templates/_component.ts
+++ generators/app/templates/_component.ts
@@ -1,9 +1,8 @@
 import {Component} from 'angular2/core'<% if (sugar.router) { %> 
 import {ROUTER_DIRECTIVES} from 'angular2/router';<% } %>
-declare var __moduleName: any;
 
 @Component({
-    moduleId: __moduleName,
+    moduleId: module.id,
     selector: '<%= selector %>',<% if (sugar.router) { %> 
     directives: [ROUTER_DIRECTIVES], <% } %><% if (sugar.html) { %> 
     templateUrl: '<%= fileName %>.html', <% } %><% if (sugar.scss) { %> 
