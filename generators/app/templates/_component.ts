27f9ddca8721e164c6363371b5fb7c0ee1ecd01a
Michael Knoch <info@michaelknoch.de>
2016-05-15 13:36:40 +0200

    update to angular rc1

--- generators/app/templates/_component.ts
+++ generators/app/templates/_component.ts
@@ -1,5 +1,5 @@
-import {Component} from 'angular2/core'<% if (sugar.router) { %> 
-import {ROUTER_DIRECTIVES} from 'angular2/router';<% } %>
+import {Component} from '@angular/core'<% if (sugar.router) { %> 
+import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';<% } %>
 
 @Component({
     moduleId: module.id,
